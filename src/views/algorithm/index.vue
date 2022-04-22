8ca90ab4fab701f3611307f31735032fd6906120
huangChaoJie <1213699301@qq.com>
2022-04-22 18:04:26 +0800

    算法调试页面

--- src/views/algorithm/index.vue
+++ src/views/algorithm/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="pd-24">
     <div>
+      <div v-show="visible">一个啊啊啊啊啊啊啊啊啊啊div</div>
       <el-input
         v-model="textarea"
         type="textarea"
@@ -20,11 +21,14 @@ export default {
   name: 'Index',
   data() {
     return {
-      textarea: ''
+      textarea: '',
+      visible: false
     }
   },
   created() {
     this.submit = this.debounce(this.submit)
+    this.onMouseMove = this.throttle(this.onMouseMove)
+    window.onmousemove = this.onMouseMove
   },
   methods: {
     debounce(cb, delay = 1000) {
@@ -39,13 +43,66 @@ export default {
         }, delay)
       }
     },
+    throttle(cb, delay = 1000) {
+      let timer = 0
+      return function() {
+        if (timer) {
+          return
+        }
+        timer = setTimeout(() => {
+          cb.apply(this, arguments)
+          timer = null
+          // const date = new Date()
+          // console.log(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds())
+        }, delay)
+      }
+    },
+    isInViewPortOfOne(el) {
+      // viewPortHeight 兼容所有浏览器写法
+      const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+      const offsetTop = el.offsetTop
+      const scrollTop = document.documentElement.scrollTop
+      const top = offsetTop - scrollTop
+      return top <= viewPortHeight
+    },
+    isInViewPort(element) {
+      const viewWidth = window.innerWidth || document.documentElement.clientWidth
+      const viewHeight = window.innerHeight || document.documentElement.clientHeight
+      const {
+        top,
+        right,
+        bottom,
+        left
+      } = element.getBoundingClientRect()
+
+      return (
+        top >= 0 &&
+        left >= 0 &&
+        right <= viewWidth &&
+        bottom <= viewHeight
+      )
+    },
     submit() {
       console.log(this.textarea)
+      this.visible = !this.visible
+    },
+    onMouseMove(event) {
+      const e = event || window.event
+      console.log('x', e.clientX, 'y', e.clientY)
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.target {
+  margin: 5px;
+  width: 20px;
+  height: 20px;
+  background: red;
+}
 </style>
